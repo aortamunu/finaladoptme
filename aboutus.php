9d4869147d8161e0d5dbb8fb4297ce9c0bf2cada
aortamunu <seemi1911@gmail.com>
2022-06-15 12:32:13 +0545

    June15commit

---
+++ aboutus.php
@@ -0,0 +1,91 @@
+<?php
+include "./includes/function.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/about.css">
+    <!-- <link rel="stylesheet" href="./styles/nav.css"> -->
+    <!-- <link rel="stylesheet" href="./styles/list.css"> -->
+    <link rel="shortcut icon" href="./public/favicon.png" type="image/x-icon">
+    <title>Adopt Me</title>
+</head>
+
+<body>
+    
+    <?php include("./partials/nav.php") ?>
+    <div class="container">
+        
+    <?php include './partials/message.php'; ?>
+
+<!-- <div class="leftbox">
+    <h1>Our Team</h1>
+</div>
+</div> -->
+<div class="aboutall">
+<div class="leftbox">
+    <h4>Our Team</h4><hr><br>
+    <!-- <hr> -->
+    <p>We are a small team of 2 members. We aspire to work together
+        in promoting adoption of animals rather than buying/selling.</p>
+        <br><h4>Meet Our Team</h4>
+        <hr><br>
+        <div class="horizontal">
+<div class="team">
+    <img src="ourteam.png"><br>
+    <a href="https://www.facebook.com/vivekheka10" class='name'>Bibek Dangol</a>
+</div>
+
+<div class="team">
+    <img src="ourteam.png"><br>
+    <a href="https://www.facebook.com/Aorta.mylove" class='name'>Seemana Karmacharya</a>
+</div>
+</div>
+
+<div class="contactlogo">
+    <p>Contact Us</p><hr>
+<a href="mailto:jukufoundation@gmail.com" class='medialogo'><img src="email.png"></a>
+<a href="https://www.facebook.com/Adopt-Me-100100012743248/" class='medialogo'><img src="facebook.png"></a>
+<a href="https://www.instagram.com/jukufoundation/" class='medialogo'><img src="instagram.png"></a>
+
+</div>
+        </div>
+
+<!-- <div class="teambox"> -->
+    <!-- <h4>Meet Our Team</h4><br> -->
+    <!-- <div class="horizontal">
+<div class="team">
+    <img src="ourteam.png"><br>
+    <a href="https://www.facebook.com/vivekheka10" class='name'>Bibek Dangol</a>
+</div>
+
+<div class="team">
+    <img src="ourteam.png"><br>
+    <a href="https://www.facebook.com/Aorta.mylove" class='name'>Seemana Karmacharya</a>
+</div>
+</div> -->
+<!-- </div> -->
+
+<div class="photoo">
+    <img src="catndog.jpg">
+</div>
+</div>
+
+    
+
+    <!-- <div class="image">
+    <img src = "bestbuddy.jpg">
+    </div> -->
+
+    
+            
+
+
+    <script src="./scripts/nav.js"></script>
+</body>
+
+</html>
